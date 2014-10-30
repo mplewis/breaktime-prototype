da408eb6846f4c768f941210fea54aff6acbd1b9
Matthew Lewis <matt@mplewis.com>
2014-10-29 22:42:21 -0500

    Initial commit

---
+++ prototype.js
@@ -0,0 +1,71 @@
+var pos = 0
+
+var imageOrder = [
+  'app-home',
+  'mins-left-25',
+  'nexus-back',
+  'mins-left-21',
+  'nexus-back',
+  'choose-break-activity',
+  'music-activity',
+  'back-to-work',
+  'set-mins-left-25',
+  'set-mins-left-10',
+  'mins-left-25',
+  'nexus-back',
+  'choose-break-activity',
+  'device-home',
+  'back-to-work'
+];
+
+function fitContent() {
+  $('.phone').height($(window).height() * 0.85);
+  $('.slide-num').css('font-size', $(window).height() * 0.05);
+}
+
+$(window).resize(function() {
+  fitContent();
+});
+
+function showSlide(pos) {
+  var imageUrlBase = imageOrder[pos];
+  if (imageUrlBase) {
+    var imageUrl = 'images/' + imageUrlBase + '.png';
+    $('.phone').attr('src', imageUrl);
+    $('.slide-num').text(pos);
+  }
+}
+
+function prevSlide() {
+  if (pos > 0) {
+    pos--;
+    showSlide(pos);
+  }
+}
+
+function nextSlide() {
+  if (pos < imageOrder.length - 1) {
+    pos++;
+    showSlide(pos);
+  }
+}
+
+$('.phone').click(nextSlide);
+
+$(document).keydown(function(e) {
+  switch(e.which) {
+    case 37: // left
+      prevSlide();
+      break;
+
+    case 39: // right
+      nextSlide();
+      break;
+
+    default: return; // exit this handler for other keys
+  }
+  e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+showSlide(pos);
+fitContent();
